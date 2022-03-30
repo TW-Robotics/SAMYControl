af1593c0c974f56cf881a8b63abc8bbe9df87205
chrisizeh <zeh.chrisi@gmail.com>
2022-03-30 12:15:30 +0200

    Parsed Loopback blocks

--- BPMNbasedController/GraphBuilder.py
+++ BPMNbasedController/GraphBuilder.py
@@ -10,8 +10,14 @@ class GraphBuilder:
         nodes = GraphBuilder.parseProcesses(dom)
         G.add_nodes_from(nodes)
 
-        edges = GraphBuilder.parseTransitions(dom, nodes)
-        G.add_edges_from(edges)
+        edges = dict(GraphBuilder.parseTransitions(dom, nodes))
+        edges = GraphBuilder.parseLoopback(G, dom, edges)
+
+        edge_list = []
+        for edge in list(edges.values()):
+            if(edge[0] in nodes and edge[1] in nodes):
+                edge_list.append(edge)
+        G.add_edges_from(edge_list)
 
         return G
 
@@ -26,15 +32,30 @@ class GraphBuilder:
         return nodes
 
     def parseTransitions(dom, nodes):
-        edges = []
+        edges = {}
 
         for i in range(3):
             elements = dom.getElementsByTagName('SamyBpmnModel:' + GraphBuilder.Types[3])
 
             for element in elements:
+                id = element.attributes['id'].value
                 source = element.attributes['sourceRef'].value
                 target = element.attributes['targetRef'].value
 
-                if(source in nodes and target in nodes):
-                    edges.append((source, target))
+                edges[id] = (source, target)
         return edges
+
+    def parseLoopback(G, dom, edges):
+        elements = dom.getElementsByTagName('SamyBpmnModel:' + GraphBuilder.Types[7])
+
+        for element in elements:
+            outgoing = GraphBuilder.getChildValue(element.getElementsByTagName('outgoing')[0])
+            for incoming in element.getElementsByTagName('incoming'):
+                incoming = GraphBuilder.getChildValue(incoming)
+                edges[incoming] = (edges[incoming][0], edges[outgoing][1])
+
+            del edges[outgoing]
+        return edges
+
+    def getChildValue(element):
+        return element.childNodes[0].nodeValue
